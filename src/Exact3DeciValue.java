a8d99de2960e4be512e10990dc65efa3b05ad638
Mayank1203 <mayankpatle1203@gmail.com>
2026-03-13 21:25:28 +0530

    TCS Coding Questions

---
+++ src/Exact3DeciValue.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Exact3DeciValue {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Double num = sc.nextDouble();
+
+        System.out.println(String.format("%.3f",num));
+    }
+}
