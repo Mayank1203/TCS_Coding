ec091158243e48b2a3c1535be38267af451ecb00
Mayank1203 <mayankpatle1203@gmail.com>
2026-04-21 20:49:11 +0530

    HollowRectangle Pattern

---
+++ src/PatternPrinting/HollowRectangle.java
@@ -0,0 +1,19 @@
+package PatternPrinting;
+
+public class HollowRectangle {
+    public static void main(String[] args){
+        int rows = 8;
+        int columns = 9;
+
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j<columns; j++){
+                if(i==0 || j==0 || i==rows-1 || j==columns-1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
