6dcd69022d5bce996377d27c704e53228ce52265
Mayank1203 <mayankpatle1203@gmail.com>
2026-04-25 23:01:00 +0530

    butterFly Pattern

---
+++ src/PatternPrinting/ButterFlyPattern.java
@@ -0,0 +1,40 @@
+package PatternPrinting;
+
+public class ButterFlyPattern {
+    public static void main(String[] args){
+        int n = 70 ;
+
+        // Upper part
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+
+            int spaces = 2*(n-i) ;
+            for(int j = 1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part
+
+        for(int i = n ; i>=1 ; i--){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+
+            int spaces = 2*(n-i) ;
+            for(int j = 1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
